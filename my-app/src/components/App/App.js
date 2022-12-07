1e9ed059bcdabf71e40712bdf968c77859b69f8b
Filippo Scelfo <scelfofilippo@gmail.com>
2022-12-07 13:19:55 +0100

    First test with react material ui

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,9 +1,17 @@
+import { ButtonATC } from '../UI/Atoms/ButtonATC/ButtonATC';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
       <h1>Hello, World!</h1>
+      <br/>
+      <br/>
+      <ButtonATC />
+      <br/>
+      <br/>
+      <Button variant="contained" size="large">Hello World</Button>
     </div>
   );
 }
---
+++ my-app/src/components/UI/Atoms/ButtonATC/ButtonATC.js
@@ -0,0 +1,7 @@
+import './buttonATC.css';
+
+export function ButtonATC() {
+  return (
+    <button className="buttonATC" data-size="large" data-theme="orange">Aggiungi al Carrello</button>
+  )
+}
