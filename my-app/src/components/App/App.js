7994adb3824b9dfb683ad2da880ca3494caaa458
LauricellaAndrea <andrealauricella97@gmail.com>
2022-12-19 17:38:47 +0100

    v1.0-RatingStar

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import DesignSystemLibrary from "../Pages/DesignSystemLibrary/DesignSystemLibrary";
 import Home from "../Pages/Home/Home";
+import RatingStar from "../Ui/RatingStar/RatingStar"
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/designsystemlibrary" element={<DesignSystemLibrary />} />
       </Routes>
+      <RatingStar/>
     </div>
   );
 }
