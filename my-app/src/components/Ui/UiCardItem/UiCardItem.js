f29b3f671a4544ea5f02b76d15f4ef276f2fb40c
Filippo Scelfo <scelfofilippo@gmail.com>
2022-12-19 18:44:48 +0100

    Add ratings and buttons cta to design library

--- my-app/src/components/Ui/UiCardItem/UiCardItem.js
+++ my-app/src/components/Ui/UiCardItem/UiCardItem.js
@@ -23,6 +23,13 @@ function UiCardItem({
       {/* Button Call to action */}
       {component === "ButtonCta1" && <ButtonCta>Scopri di più</ButtonCta>}
       {component === "ButtonCta2" && <ButtonCta color="white">Scopri di più</ButtonCta>}
+      {component === "ButtonCta3" && <ButtonCta color="black" theme="outline">Scopri di più</ButtonCta>}
+      {component === "ButtonCta4" && <ButtonCta color="black" shape="pill">Scopri di più</ButtonCta>}
+      {component === "ButtonCta5" && <ButtonCta color="white" shape="pill">Scopri di più</ButtonCta>}
+      {component === "ButtonCta6" && <ButtonCta color="white" shape="pill" theme="outline">Scopri di più</ButtonCta>}
+      {component === "ButtonCta7" && <ButtonCta color="black" size="small">Scopri di più</ButtonCta>}
+      {component === "ButtonCta8" && <ButtonCta color="black" size="medium">Scopri di più</ButtonCta>}
+      {component === "ButtonCta9" && <ButtonCta color="black" size="large">Scopri di più</ButtonCta>}
     </div>
   )
 }
