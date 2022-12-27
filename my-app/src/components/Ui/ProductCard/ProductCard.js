65c9d683a4c0351cf993c4a8bd1917b2ac64017f
Gianfranco D'Angelo <Gianfranco.dangelo94@gmail.com>
2022-12-27 12:15:21 +0100

    card con rating v1 + prop size su rating comp

--- my-app/src/components/Ui/ProductCard/ProductCard.js
+++ my-app/src/components/Ui/ProductCard/ProductCard.js
@@ -1,5 +1,7 @@
 import ButtonAtc from "../ButtonAtc/ButtonAtc";
-import "../ProductCard/productCard.css"
+import RatingStar from '../../Ui/RatingStar/RatingStar'
+import "../ProductCard/productCard.css";
+
 
 // novità=true fa comparire il badge novità,impostare uno sconto fa comparire il badge sconto con la percentuale indicata
 // e svolge il calcolo dello sconto partendo dal prezzo, linkprodotto abilità il collegamento con la pagina del prodotto sia per il titolo
@@ -10,11 +12,12 @@ function ProductCard({
     sconto=0,
     linkimmagine="https://www.lego.com/cdn/cs/set/assets/bltdcfa4924bc8abd0a/21335.png?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=1.5",
     titolo="Product Title",
+    rating=0,
     prezzo,
-    template= "template1",
-    dataskeleton="false",
     linkprodotto="",
     onClick="",
+    template= "template1",
+    dataskeleton="false",
 }) {
   return (
     <div
@@ -45,6 +48,10 @@ function ProductCard({
         <a href={linkprodotto} className={"productCard-title"}>
           {titolo}
         </a>
+        <div className={"productCard-starRating-box"}>
+          {/* <!-- Component Rating --> */}
+          <RatingStar score={rating} size="small" />
+        </div>
         {sconto > 0 ? (
           <div className={"productCard-content-price"}>
               <h6 className={"productCard-price-old"}> €{prezzo} </h6>
@@ -53,7 +60,6 @@ function ProductCard({
         ) : (
           <h6 className={"productCard-price"}>€{prezzo}</h6>
         )}
-
         <div className={"productCard-button"}>
           {/* <!-- Component Add to cart button --> */}
           <ButtonAtc color="primary" size="medium" onClick={onClick}>Add to cart</ButtonAtc>
--- my-app/src/components/Ui/RatingStar/RatingStar.js
+++ my-app/src/components/Ui/RatingStar/RatingStar.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import Rating from "@mui/material/Rating";
 
-export default function BasicRating({score}) {
+export default function BasicRating({score, size}) {
   // const [value, setValue] = React.useState(score);
   return (
-    <Rating name="read-only" value={score} readOnly />
+    <Rating name="read-only" value={score} size={size} readOnly />
   );
 }
