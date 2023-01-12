31c0dcc85acecbd4bf9d217b09a21d97ee83ecac
Filippo Scelfo <scelfofilippo@gmail.com>
2023-01-12 17:48:27 +0100

    Update design library

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,17 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import DesignSystemLibrary from "../Pages/DesignSystemLibrary/DesignSystemLibrary";
 import Home from "../Pages/Home/Home";
-import { Banner } from "../Ui/Banner/Banner";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/designsystemlibrary" element={<DesignSystemLibrary />} />
       </Routes>
-      <Banner background="black"/>
     </div>
   );
 }
